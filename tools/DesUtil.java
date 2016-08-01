29fdf91167b6507eb1047c2b9a53c4e28ccd0359
zhihaibang <zhihaibang1@jd.com>
2016-08-01 18:03:32 +0800

    des加密解密

---
+++ tools/DesUtil.java
@@ -0,0 +1,73 @@
+package zhb.des;
+
+import java.security.Key;
+import java.security.spec.AlgorithmParameterSpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import it.sauronsoftware.base64.Base64;
+ 
+public class DesUtil {
+
+	 public static final String ALGORITHM_DES = "DES/CBC/PKCS5Padding";
+	 private static byte[] DESIV = { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+
+	 /**
+	  * DES算法，加密
+	  *
+	  * @param data 待加密字符串
+	  * @param key 加密私钥，长度不能够小于8位
+	  * @return 加密后的字符串，一般结合Base64编码使用
+	  * @throws CryptException 异常
+	  */
+	 public static String encrypt(String key,String data) throws Exception
+	 {
+        DESKeySpec dks = new DESKeySpec(key.getBytes());
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+	   //key的长度不能够小于8位字节
+	   Key secretKey = keyFactory.generateSecret(dks);
+	   Cipher cipher = Cipher.getInstance(ALGORITHM_DES);
+	   IvParameterSpec iv = new IvParameterSpec(DESIV);
+	   AlgorithmParameterSpec paramSpec = iv;
+	   cipher.init(Cipher.ENCRYPT_MODE, secretKey, paramSpec);
+	   byte[] bytes = cipher.doFinal(data.getBytes());
+	   return new String(Base64.encode(bytes));
+	 }
+	  
+	 /**
+	  * DES算法，解密
+	  *
+	  * @param data 待解密字符串
+	  * @param key 解密私钥，长度不能够小于8位
+	  * @return 解密后的字符串
+	  * @throws Exception 异常
+	  */
+	 public static String decrypt(String key,String data) throws Exception
+	 {
+	        DESKeySpec dks = new DESKeySpec(key.getBytes());
+	        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+		   //key的长度不能够小于8位字节
+		   Key secretKey = keyFactory.generateSecret(dks);
+		   Cipher cipher = Cipher.getInstance(ALGORITHM_DES);
+		   IvParameterSpec iv = new IvParameterSpec(DESIV);
+		   AlgorithmParameterSpec paramSpec = iv;
+		   cipher.init(Cipher.DECRYPT_MODE, secretKey,paramSpec);	   
+		   byte[] bytes =  cipher.doFinal(Base64.decode(data.getBytes()));
+		   return new String(bytes);
+	 }
+	  
+	 public static void main(String[] args) throws Exception
+	 {
+		String str = "hello world,zhb!";
+		String key = "very important data";
+		String m_data = encrypt(key,str);
+		
+		System.out.println(str);
+	    System.out.println(m_data);
+	    
+		String data = decrypt(key,m_data);
+	    
+	    System.out.println(data);
+	 }
+}
---
+++ tools/des.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class DES {
+
+    var $key;
+    var $iv; //偏移量
+  
+    public function __construct($key, $iv=0)
+    {
+        $this->key = $key;
+        if($iv == 0)
+        {
+            $this->iv = $key;
+        }
+        else
+        {
+            $this->iv = $iv;
+        }
+    }
+  
+    //加密
+    public function encrypt($str)
+    {       
+        $size = mcrypt_get_block_size ( MCRYPT_DES, MCRYPT_MODE_CBC);
+        $str = $this->pkcs5Pad ($str, $size);
+        $data=mcrypt_cbc(MCRYPT_DES, $this->key, $str, MCRYPT_ENCRYPT, $this->iv);
+        return base64_encode($data);
+    }
+  
+    //解密
+    public function decrypt($str)
+    {
+        $str = base64_decode ($str);
+        $str = mcrypt_cbc(MCRYPT_DES, $this->key, $str, MCRYPT_DECRYPT, $this->iv);
+        $str = $this->pkcs5Unpad($str);
+        return $str;
+    }
+  
+    private function pkcs5Pad($text, $blocksize)
+    {
+        $pad = $blocksize - (strlen ($text) % $blocksize);
+        return $text . str_repeat (chr ($pad), $pad);
+    }
+  
+    private function pkcs5Unpad($text)
+    {
+        $pad = ord ( $text {strlen ($text) - 1} );
+        if ($pad > strlen ($text))
+            return false;
+        if (strspn ( $text, chr ($pad), strlen ( $text ) - $pad ) != $pad)
+            return false;
+        return substr ( $text, 0, - 1 * $pad );
+    }
+}
